684e17cd8ab87dd05f12ef9c97089e1352122c52
Peter <ptk726@gmail.com>
2017-12-04 22:50:27 -0500

    links and buttons

--- models/wine.js
+++ models/wine.js
@@ -5,8 +5,8 @@ const wineSchema = mongoose.Schema({
   name_of_winery: { type: String, require: true },
   year: { type: String, require: true },
   region: { type: String, require: true },
+  country: String,
   url: String,
-  file: String, //to revisit
   submitted_by: { type: String, require: true }
 });
 
